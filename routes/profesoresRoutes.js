0f10be980a5b9faa23a0da1d0e7123b2c80c4f7e
Jaime Martret <jaime.martret@gmail.com>
2026-06-22 11:57:40 +0200

    fix: add missing professor mutation routes

--- routes/profesoresRoutes.js
+++ routes/profesoresRoutes.js
@@ -12,7 +12,11 @@ const authMiddleware = require("../controllers/middlewares/authMiddleware");
 const handleValidation = require("../controllers/middlewares/handleValidation");
 const profesorValidator = require("../controllers/middlewares/validators/profesorValidator");
 
+
 router.get("/", authMiddleware, getProfesores);
 router.get("/:id", authMiddleware, getProfesor);
+router.post("/", authMiddleware, profesorValidator, handleValidation, createProfesor);
+router.put("/:id", authMiddleware, profesorValidator, handleValidation, updateProfesor);
+router.delete("/:id", authMiddleware, deleteProfesor);
 
 module.exports = router;
